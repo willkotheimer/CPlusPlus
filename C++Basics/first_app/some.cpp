c845e2ffedc843a0cd3646e231e13c2c21611c5c
WillKotheimer <wkotheimer@gmail.com>
2019-07-17 21:29:23 -0500

    Initial Push

---
+++ C++Basics/first_app/some.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main() 
+{
+
+	int a = 5;
+	cout<<"Enter an integer: ";
+	cin>>a;
+	cout<<"doing +3 to your integer gives "<<a+3;
+
+return 0;
+
+}
